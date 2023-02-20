a6bae5b2cb9ce273ca1f7edb86b42f64f82695c0
MariamAdisa <drmariamadisa@gmail.com>
2023-02-20 13:30:36 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase then uppercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+
+{
+	int i;
+
+	int j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
