8da8bd6c844cd8ce70332a1fe2170bed967f9c11
MariamAdisa <drmariamadisa@gmail.com>
2023-02-28 20:34:39 -0800

    prints haif of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: inoust string
+ * Return: second half of the string
+ */
+
+void puts_half(char *str)
+{
+	int length = 0, i, n;
+
+	while (str[length] > 0)
+		length++;
+
+	if (length % 2 == 0)
+		n = length / 2;
+
+	else
+		n = (length + 1) / 2;
+
+	for (i = n; i < length; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
